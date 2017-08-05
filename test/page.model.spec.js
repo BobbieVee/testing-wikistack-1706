43e8d4ba2eea5bed565daf4ee25fcbacda6ae9c9
panda <panda@panda.local>
2017-08-05 18:22:19 -0400

    working on findSimilar

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -21,22 +21,102 @@ describe('Page model', function () {
       });
     });
     describe('renderedContent', function () {
-      it('converts the markdown-formatted content into HTML');
+      it('converts the markdown-formatted content into HTML', function(){
+        page.content = 'subtitle';
+        expect(page.renderedContent).to.equal('<p>subtitle</p>\n');
+      });
     });
   });
 
   describe('Class methods', function () {
+    before(function (done) {
+      Page.create({
+          title: 'foo',
+          content: 'bar',
+          tags: ['foo', 'bar']
+        })
+        .then(function () {
+          done();
+        })
+        .catch(done);
+    });
+    
     describe('findByTag', function () {
-      it('gets pages with the search tag');
-      it('does not get pages without the search tag');
+      afterEach(function(done){
+        Page.sync({force: true})
+        .then(function(){
+          done()
+        })
+        .catch(done);
+      })
+      it('gets pages with the search tag', function(done){
+        Page.findByTag('foo')
+        .then(function(result){
+           expect(result).to.have.lengthOf(1);
+          done();
+        })
+        .catch(done);     
+      });
+      it('does not get pages without the search tag', function(done){
+        Page.findByTag('chicken')
+        .then(function(result){
+          expect(result).to.have.length(0);
+          done()
+        })
+        .catch(done);
+      });
     });
   });
 
   describe('Instance methods', function () {
+    
+    beforeEach(function (done) {
+      Promise.all([ 
+        Page.create({
+          title: 'foo',
+          content: 'bar',
+          tags: ['foo', 'baz']
+        }),
+        Page.create({
+          title: 'foo 2',
+          content: 'bar 2',
+          tags: ['foo', 'bar']
+        }),
+        Page.create({
+          title: 'foo 3',
+          content: 'bar 3',
+          tags: ['foo', 'bar', 'baz']
+        })
+      ])
+      .then(function(){
+        done();
+      })
+      .catch(done);
+     
+    });
+    
+    
     describe('findSimilar', function () {
-      it('never gets itself');
-      it('gets other pages with any common tags');
-      it('does not get other pages without any common tags');
+      it('never gets itself', function(done){
+        Page.findOne({
+          where: {
+            title: 'foo'
+          }
+        })
+        .then(function(page){
+          console.log("page findSimilar =", page.dataValues)
+          expect(page.dataValues.findSimilar).to.equal(2);
+          
+        })
+          
+        expects()
+      });
+      it('gets other pages with any common tags', function(done){
+        
+      });
+      it('does not get other pages without any common tags', function(done){
+        
+      });
     });
   });
 
